f2d8c20e6ac8de78f2888e097187bc8d6fc27baa
Mukhtiar84 <mukhtiaralikmc@gmail.com>
2023-12-12 10:23:11 +0500

    switch

---
+++ switch.js
@@ -0,0 +1,35 @@
+/*let day_of_week = prompt('Enter a day of week').toLowerCase()
+switch(true){
+    case day_of_week == 'monday' || day_of_week == 'mon':
+        console.log("Gym chest day")
+        break
+    case day_of_week == 'tuesday' || day_of_week == 'tue':
+        console.log("Wings day")
+        break
+    case day_of_week == 'wednesday' || day_of_week == 'wed':
+        console.log("Bicept day")
+        break
+    case day_of_week == 'thursday' || day_of_week == 'thu':
+        console.log("Tricept")
+        break
+    case day_of_week == 'friday' || day_of_week == 'fri':
+        console.log("Shoulder day")
+        break
+    case day_of_week == 'saturday' || day_of_week == 'sat':
+        console.log("Mix day")
+        break
+    case day_of_week == 'sunday' || day_of_week == 'sun':
+        console.log("Off day")
+        break
+    default:
+        console.log("Enter a valid day")    
+}*/
+
+//####### Date Module #########
+ /*var today = new Date();
+ alert(today);
+ alert(today.getDate())
+ alert(today.getDay())
+
+var Ali_valima = new Date("December 12, 2035, 5:00");
+alert(Ali_valima);*/
